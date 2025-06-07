cdf42fb3c8aa181f473bc18ca26d0db1094929d6
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-06-08 01:44:56 +0500

    build fixed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.module.rules.push({
-      test: /@supabase\/realtime-js/,
-      use: 'ignore-loader'
-    })
+    // Only add the rule if we're in production
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rules.push({
+        test: /@supabase\/realtime-js/,
+        use: 'ignore-loader'
+      })
+    }
     
     // Optimize webpack cache
     config.cache = {
