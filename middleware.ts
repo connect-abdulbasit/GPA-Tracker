db48a4a4972729495b79cf6d22c84ef5e97599b8
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-06-07 18:21:57 +0500

    updated the UI and convert whole thing into next js

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/semesters(.*)", "/grade-chart(.*)"])
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    const { userId } = await auth()
+    
+    if (!userId) {
+      return NextResponse.redirect(new URL("/", req.url))
+    }
+  }
 })
 
 export const config = {
