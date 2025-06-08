e523e25bad89f34f69655614fad6e58f96d43f36
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-06-08 15:46:54 +0500

    update the loader

--- app/(dashboard)/grade-chart/loading.tsx
+++ app/(dashboard)/grade-chart/loading.tsx
@@ -1,8 +1,9 @@
 "use client"
+import { HashLoader } from "react-spinners"
 export default function Loading() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+      <HashLoader color="#4F46E5" />
     </div>
   );
 } 
