50f6ecdc9277aa634f10f358787d58a77c41f5c0
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-06-08 15:53:38 +0500

    removed loading from folders

--- app/(dashboard)/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { HashLoader } from "react-spinners"
-export default function Loading() {
-  return (
-    <div className="flex w-full items-center justify-center py-8">
-      <HashLoader color="#4F46E5" />
-    </div>
-  );
-} 
