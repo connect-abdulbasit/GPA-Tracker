d0853549dde93faa7707a942d8f162104934755b
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-06-08 14:27:56 +0500

    config errors fixed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.ignoreWarnings = [
-      {
-        module: /@supabase\/realtime-js/,
-        message: /the request of a dependency is an expression/,
-      },
-    ];
-    return config;
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@clerk/nextjs']
-  }
+  serverExternalPackages: ['@clerk/nextjs']
 };
 
 module.exports = nextConfig;
