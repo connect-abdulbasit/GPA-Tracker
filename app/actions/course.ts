59c0638ce5234c4b03f28f60481d11e1fe5ec61b
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-06-09 19:47:18 +0500

    updation of the sgpa on deletion

--- app/actions/course.ts
+++ app/actions/course.ts
@@ -1,22 +1,29 @@
-"use server"
-import { calculateSGPA } from "@/lib/gpa-calculations"
-import { db } from "@/src/db"
-import { coursesTable, semestersTable } from "@/src/db/schema"
-import { and, eq, ne } from "drizzle-orm"
-export const addCourse = async (semesterId: string, userId: string, name: string, creditHours: number, gpa: number, type: string) => {
+"use server";
+import { calculateSGPA } from "@/lib/gpa-calculations";
+import { db } from "@/src/db";
+import { coursesTable, semestersTable } from "@/src/db/schema";
+import { and, eq, ne } from "drizzle-orm";
+export const addCourse = async (
+  semesterId: string,
+  userId: string,
+  name: string,
+  creditHours: number,
+  gpa: number,
+  type: string
+) => {
   let courseInsert;
 
   if (type !== "non-credit") {
     const existingCourse = await db
       .select({
         credit_hours: coursesTable.credit_hours,
-        gpa: coursesTable.gpa
+        gpa: coursesTable.gpa,
       })
       .from(coursesTable)
       .where(
         and(
           eq(coursesTable.semester_id, semesterId),
-          eq(coursesTable.user_id, userId), 
+          eq(coursesTable.user_id, userId),
           eq(coursesTable.active, true),
           ne(coursesTable.type, "non-credit")
         )
@@ -27,26 +34,29 @@ export const addCourse = async (semesterId: string, userId: string, name: string
       ...existingCourse,
       {
         credit_hours: creditHours,
-        gpa: gpa
-      }
+        gpa: gpa,
+      },
     ]);
 
     const [, insertResult] = await Promise.all([
-      db.select({
-        total_credits: semestersTable.total_credits
-      })
-      .from(semestersTable)
-      .where(eq(semestersTable.id, semesterId))
-      .then(([result]) => 
-        db.update(semestersTable)
-          .set({
-            gpa: sgpa,
-            total_credits: result.total_credits + creditHours,
-          })
-          .where(eq(semestersTable.id, semesterId))
-          .execute()
-      ),
-      db.insert(coursesTable)
+      db
+        .select({
+          total_credits: semestersTable.total_credits,
+        })
+        .from(semestersTable)
+        .where(eq(semestersTable.id, semesterId))
+        .then(([result]) =>
+          db
+            .update(semestersTable)
+            .set({
+              gpa: sgpa,
+              total_credits: result.total_credits + creditHours,
+            })
+            .where(eq(semestersTable.id, semesterId))
+            .execute()
+        ),
+      db
+        .insert(coursesTable)
         .values({
           semester_id: semesterId,
           user_id: userId,
@@ -55,64 +65,89 @@ export const addCourse = async (semesterId: string, userId: string, name: string
           gpa: gpa,
           type: type,
         })
-        .execute()
+        .execute(),
     ]);
 
     courseInsert = insertResult;
-
   } else {
-    [,courseInsert] = await Promise.all([
-    db.select({
-      total_credits: semestersTable.total_credits
-    })
-    .from(semestersTable)
-    .where(eq(semestersTable.id, semesterId))
-    .then(([result]) => 
-      db.update(semestersTable)
-        .set({
-          total_credits: result.total_credits + creditHours,
+    [, courseInsert] = await Promise.all([
+      db
+        .select({
+          total_credits: semestersTable.total_credits,
         })
+        .from(semestersTable)
         .where(eq(semestersTable.id, semesterId))
-        .execute()
-    ),
-    db
-      .insert(coursesTable)
-      .values({
-        semester_id: semesterId,
-        user_id: userId,
-        name,
-        credit_hours: creditHours,
-        gpa: gpa,
-        type: type,
-      })
-      .execute()
-    ])
+        .then(([result]) =>
+          db
+            .update(semestersTable)
+            .set({
+              total_credits: result.total_credits + creditHours,
+            })
+            .where(eq(semestersTable.id, semesterId))
+            .execute()
+        ),
+      db
+        .insert(coursesTable)
+        .values({
+          semester_id: semesterId,
+          user_id: userId,
+          name,
+          credit_hours: creditHours,
+          gpa: gpa,
+          type: type,
+        })
+        .execute(),
+    ]);
   }
 
   return courseInsert;
-}
+};
 export const deleteCourse = async (courseId: string) => {
-  const [course] = await db.update(coursesTable).set({
-    active: false,
-  }).where(and(eq(coursesTable.id, courseId), eq(coursesTable.active, true))).returning();
+  let sgpa;
+  const [course] = await db
+    .update(coursesTable)
+    .set({
+      active: false,
+    })
+    .where(and(eq(coursesTable.id, courseId), eq(coursesTable.active, true)))
+    .returning();
+  if (course.type !== "non-credit") {
+    const existingCourse = await db
+    .select({
+      credit_hours: coursesTable.credit_hours,
+      gpa: coursesTable.gpa,
+    })
+    .from(coursesTable)
+    .where(
+      and(
+        eq(coursesTable.semester_id, course.semester_id),
+        eq(coursesTable.user_id, course.user_id),
+        eq(coursesTable.active, true),
+        ne(coursesTable.type, "non-credit")
+      )
+    )
+    .execute();
 
-  const existingCourses = await db.select({
-    credit_hours: coursesTable.credit_hours, 
-    gpa: coursesTable.gpa
-  })
-  .from(coursesTable)
-  .where(and(
-    eq(coursesTable.semester_id, course.semester_id),
-    eq(coursesTable.user_id, course.user_id), 
-    eq(coursesTable.active, true)
-  ))
-  .execute();
-
-  const sgpa = calculateSGPA(existingCourses);
-  const totalCredits = existingCourses.reduce((sum, course) => sum + course.credit_hours, 0);
-
-  await db.update(semestersTable).set({
-    gpa: sgpa,
-    total_credits: totalCredits,
-  }).where(eq(semestersTable.id, course.semester_id)).execute();
-}
+    sgpa = calculateSGPA([
+      ...existingCourse,
+    ]);
+  
+  }
+    const [totalCredits] = await db
+      .select({
+        total_credits: semestersTable.total_credits,
+        gpa: semestersTable.gpa,
+      })
+      .from(semestersTable)
+      .where(eq(semestersTable.id, course.semester_id))
+      .execute();
+    await db
+      .update(semestersTable)
+      .set({
+        gpa: sgpa ?? totalCredits.gpa,
+        total_credits: totalCredits.total_credits - course.credit_hours,
+      })
+      .where(eq(semestersTable.id, course.semester_id))
+      .execute();
+  
+};
