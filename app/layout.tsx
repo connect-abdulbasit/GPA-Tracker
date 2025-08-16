b4950e9549e076bdecaa4fb9290cb9ccc73d8898
connect-abdulbasit <contact.abdulbasit.cs@gmail.com>
2025-08-16 13:01:11 +0500

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 import { Suspense } from "react";
 import Loading from "./loading";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({
               </Suspense>
             <Toaster />
           </ThemeProvider>
+          <Analytics />
       </body>
     </html>
   );
